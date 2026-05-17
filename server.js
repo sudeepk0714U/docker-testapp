1a193f080ce96e2b268f92835504acc72bcc36e4
Sudeep Kumar k <officialsudeepk@gmail.com>
2026-05-17 20:31:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,47 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 const MongoClient = require("mongodb").MongoClient;
 
 const PORT = 5050;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const MONGO_URL = "mongodb://admin:qwerty@localhost:27017";
+const MONGO_URL = "mongodb://admin:qwerty@mongo:27017";
+
 const client = new MongoClient(MONGO_URL);
 
-//GET all users
-app.get("/getUsers", async (req, res) => {
-    await client.connect(URL);
-    console.log('Connected successfully to server');
+async function startServer() {
+    try {
+        await client.connect();
+        console.log("Connected successfully to MongoDB");
 
-    const db = client.db("apnacollege-db");
-    const data = await db.collection('users').find({}).toArray();
-    
-    client.close();
-    res.send(data);
-});
+        const db = client.db("apnacollege-db");
 
-//POST new user
-app.post("/addUser", async (req, res) => {
-    const userObj = req.body;
-    console.log(req.body);
-    await client.connect(URL);
-    console.log('Connected successfully to server');
+        // GET all users
+        app.get("/getUsers", async (req, res) => {
+            const data = await db.collection("users").find({}).toArray();
+            res.send(data);
+        });
 
-    const db = client.db("apnacollege-db");
-    const data = await db.collection('users').insertOne(userObj);
-    console.log(data);
-    console.log("data inserted in DB");
-    client.close();
-});
+        // POST new user
+        app.post("/addUser", async (req, res) => {
+            const userObj = req.body;
 
+            const data = await db.collection("users").insertOne(userObj);
 
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
-});
+            console.log("Data inserted");
+
+            res.send("User added successfully");
+        });
+
+        app.listen(PORT, "0.0.0.0", () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+startServer();
